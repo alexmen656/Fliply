9970f92389f1c562318705b442f4e54b3f071d75
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-04 15:00:59 +0200

    feat: Initialize Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eu.controlcenter.fliply',
+  appName: 'fliply',
+  webDir: 'dist'
+};
+
+export default config;
